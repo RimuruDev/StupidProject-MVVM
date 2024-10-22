2bacd3a692e5dc727b04e7e8da7828eebc35227a
Rimuru <85500556+RimuruDev@users.noreply.github.com>
2024-10-22 18:40:01 +0400

    Added TransitionBridge

--- StupidProject-MVVM/Assets/Internal/Codebase/Infrastructure/AssetManagement/AssetPath.cs
+++ StupidProject-MVVM/Assets/Internal/Codebase/Infrastructure/AssetManagement/AssetPath.cs
@@ -3,5 +3,6 @@ namespace AbyssMoth.Internal.Codebase.Infrastructure.AssetManagement
     public static class AssetPath
     {
         public static readonly string UIRoot = "StaticData/UIRoot";
+        public static readonly string EventSystem = "StaticData/EventSystem";
     }
 }
---
+++ StupidProject-MVVM/Assets/Internal/Codebase/Infrastructure/Utilities/SceneName.cs
@@ -0,0 +1,9 @@
+namespace AbyssMoth.Internal.Codebase.Infrastructure.Utilities
+{
+    public static class SceneName
+    {
+        public static readonly string BOOT = "Boot";
+        public static readonly string GAMEPLAY = "Gameplay";
+        public static readonly string TRANSITION_BRIDGE = "TransitionBridge";
+    }
+}
