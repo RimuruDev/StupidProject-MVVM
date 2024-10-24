7b621f67e649f3c23d1975755dad39fe9c2bde9b
Rimuru <85500556+RimuruDev@users.noreply.github.com>
2024-10-24 23:04:18 +0400

    Update SomeCommandService.cs

--- StupidProject-MVVM/Assets/Internal/Codebase/Infrastructure/Services/SomeCommandService.cs
+++ StupidProject-MVVM/Assets/Internal/Codebase/Infrastructure/Services/SomeCommandService.cs
@@ -1,21 +1,32 @@
 using System;
 using AbyssMoth.Internal.Codebase.Runtime.Gameplay.State.GameStateProviders;
-using AbyssMoth.Internal.Codebase.Runtime.Gameplay.State.Root;
+using ObservableCollections;
 using UnityEngine;
+using R3;
 
 namespace AbyssMoth.Internal.Codebase.Infrastructure.Services
 {
     public class SomeCommandService : IDisposable
     {
-        private readonly GameStateProxy gameState;
-        
+        private readonly IDisposable addSubscription;
+        private readonly IDisposable removeSubscription;
+
         public SomeCommandService(IGameStateProvider gameState)
         {
             Debug.Log($"{GetType().Name} has been created!");
-            
+
             gameState.GameState.Buildings.ForEach(x => Debug.Log($"Building: {x}"));
+
+            addSubscription = gameState.GameState.Buildings.ObserveAdd().Subscribe(e => Debug.Log($"Building: {e.Value}"));
+            removeSubscription = gameState.GameState.Buildings.ObserveRemove().Subscribe(e => Debug.Log($"Building: {e.Value}"));
         }
-        
-        public void Dispose() => Debug.Log($"<color=yellow>{GetType().Name} Dispose!</color>");
+
+        public void Dispose()
+        {
+            Debug.Log($"<color=yellow>{GetType().Name} Dispose!</color>");
+
+            addSubscription?.Dispose();
+            removeSubscription?.Dispose();
+        }
     }
 }
