f67757ef25414481f8a2e10abec13d644e2ae7cd
Rimuru <85500556+RimuruDev@users.noreply.github.com>
2024-10-24 23:44:09 +0400

    Update IGameStateProvider.cs

--- StupidProject-MVVM/Assets/Internal/Codebase/Runtime/Gameplay/State/GameStateProviders/IGameStateProvider.cs
+++ StupidProject-MVVM/Assets/Internal/Codebase/Runtime/Gameplay/State/GameStateProviders/IGameStateProvider.cs
@@ -11,20 +11,46 @@ namespace AbyssMoth.Internal.Codebase.Runtime.Gameplay.State.GameStateProviders
         public GameStateProxy GameState { get; }
 
         /// <summary>
+        ///  Защищенная прокси настроек игры.
+        /// </summary>
+        public GameSettingsStateProxy SettingsState { get; }
+
+        
+        
+        /// <summary>
         /// Сохраняет состояние игры.
         /// </summary>
         /// <returns>
-        /// true - Успешно сохранено.
-        /// false - Не получилось сохранить.
+        /// true - Состояние игры -> Успешно сохранено.
+        /// false - Состояние игры -> Не получилось сохранить.
         /// </returns>
         public Observable<bool> SaveGameState();
 
+        /// <summary>
+        /// Сохраняет настройки игры.
+        /// </summary>
+        /// <returns>
+        /// true - Настройки игры -> Успешно сохранено.
+        /// false - Настройки игры -> Не получилось сохранить.
+        /// </returns>
+        public Observable<bool> SaveSettingsState();
+
+        
+        
         /// <summary>
         /// Загружает состояние игры.
         /// </summary>
         /// <returns>Защищенная прокси состояния игры.</returns>
         public Observable<GameStateProxy> LoadGameState();
 
+        /// <summary>
+        /// Загружает настройки игры.
+        /// </summary>
+        /// <returns>Защищенная прокси настройки игры.</returns>
+        public Observable<GameSettingsStateProxy> LoadSettingsState();
+
+        
+        
         /// <summary>
         /// Сброс состояния игры.
         /// </summary>
@@ -33,5 +59,14 @@ namespace AbyssMoth.Internal.Codebase.Runtime.Gameplay.State.GameStateProviders
         /// false - Не получилось сбросить состояние игры.
         /// </returns>
         public Observable<bool> ResetGameState();
+
+        /// <summary>
+        /// Сброс настроек игры.
+        /// </summary>
+        /// <returns>
+        /// true - Настройки игры успешно сброшено.
+        /// false - Не получилось сбросить настройки игры.
+        /// </returns>
+        public Observable<bool> ResetSettingsState();
     }
 }
