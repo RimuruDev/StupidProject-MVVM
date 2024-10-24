af81a5fbacb2026fc95edffa11ccfc7dd3a2fe70
Rimuru <85500556+RimuruDev@users.noreply.github.com>
2024-10-24 23:12:32 +0400

    Update SomeCommandService.cs

--- StupidProject-MVVM/Assets/Internal/Codebase/Infrastructure/Services/SomeCommandService.cs
+++ StupidProject-MVVM/Assets/Internal/Codebase/Infrastructure/Services/SomeCommandService.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using AbyssMoth.Internal.Codebase.Runtime.Gameplay.State.Buildings;
 using AbyssMoth.Internal.Codebase.Runtime.Gameplay.State.GameStateProviders;
 using ObservableCollections;
 using UnityEngine;
@@ -8,17 +10,26 @@ namespace AbyssMoth.Internal.Codebase.Infrastructure.Services
 {
     public class SomeCommandService : IDisposable
     {
+        private readonly IGameStateProvider gameState;
         private readonly IDisposable addSubscription;
         private readonly IDisposable removeSubscription;
 
         public SomeCommandService(IGameStateProvider gameState)
         {
+            this.gameState = gameState;
+            
             Debug.Log($"{GetType().Name} has been created!");
 
             gameState.GameState.Buildings.ForEach(x => Debug.Log($"Building: {x}"));
 
             addSubscription = gameState.GameState.Buildings.ObserveAdd().Subscribe(e => Debug.Log($"Building: {e.Value}"));
             removeSubscription = gameState.GameState.Buildings.ObserveRemove().Subscribe(e => Debug.Log($"Building: {e.Value}"));
+
+            AddBuilding("Merunya");
+            AddBuilding("Megumin");
+            
+            RemoveBuilding("Merunya");
+            RemoveBuilding("Megumin");
         }
 
         public void Dispose()
@@ -27,6 +38,27 @@ namespace AbyssMoth.Internal.Codebase.Infrastructure.Services
 
             addSubscription?.Dispose();
             removeSubscription?.Dispose();
+        }
+
+        private void AddBuilding(string id)
+        {
+            var newBuilding = new BuildingEntity
+            {
+                TypeId = id
+            };
+
+            var proxy = new BuildingEntityProxy(newBuilding);
+            gameState.GameState.Buildings.Add(proxy);
+        }  
+        
+        private void RemoveBuilding(string id)
+        {
+            var buildingForRemove =gameState.GameState.Buildings.FirstOrDefault(x => x.TypeId == id);
+
+            if (buildingForRemove != null)
+            {
+                gameState.GameState.Buildings.Remove(buildingForRemove);
+            }
         }
     }
 }
